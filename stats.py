a46357a17319206b1813d5c36b114c5879db441b
vincent.casgrain@gmail.com <vincent.casgrain@gmail.com>
2019-11-07 13:58:41 -0500

    more fixe and cleaning up for python 3.8 - stats

--- stats.py
+++ stats.py
@@ -9,7 +9,13 @@ import math
 class Stats:
 	__stat_type = ['Wins','Busts','Money','Bust_Slope','Bet_size']
 
-	def __init__(self,game):
+	def __init__(self, game):
+		"""
+		The stat class collect every stat about the game.
+		TODO Complete revamp. no longer give a specific stats to the player, the player get all the stats then choose
+		what to do with it
+		:param game:
+		"""
 
 		if not game:
 			print("Instanciation of a Game Class requiere a game Class")
@@ -37,9 +43,14 @@ class Stats:
 		self.bjgame = game
 		self.numb_of_players = len(self.bjgame.players)
 
+		# When calculating the slope, this is the Dx (over how many point you do the slope)
 		self.slope_width = 10 #default
 
 	def update_stats(self):
+		"""
+		Scan all the player for thiers stats
+		:return: NA
+		"""
 
 		for players in self.bjgame.players:
 			self.wins_buffer.append(players.victory)
@@ -49,19 +60,33 @@ class Stats:
 
 			self.differential(self.bust_buffer,self.bust_slopes_buffer,players.bust_count)
 
-	def differential(self,data_buffer,data_slopes_buffer,player_var):
+	def differential(self, data_buffer, data_slopes_buffer, player_var):
+		"""
+		Do a derevative of a specific stats
+		:param data_buffer: A data array / list
+		:param data_slopes_buffer: the data array / list of slopes? Not ever sure myself
+		:param player_var: The variable to differentiate
+		:return: NA
+		"""
 
-		if len(data_buffer)>self.numb_of_players * self.slope_width:
-			data_slopes_buffer.append( self.slope(player_var,data_buffer[-((self.numb_of_players*self.slope_width)+1)]))
+		#not sure what this do anymore
+		if len(data_buffer) > self.numb_of_players * self.slope_width:
+			data_slopes_buffer.append(self.slope(player_var, data_buffer[-((self.numb_of_players * self.slope_width)+1)]))
 
-		elif len(data_buffer)<=self.numb_of_players:
+		elif len(data_buffer) <= self.numb_of_players:
 			data_slopes_buffer.append(0)
 
 		else:
-			data_slopes_buffer.append(self.slope(player_var,data_buffer[-(self.numb_of_players+1)]))
+			data_slopes_buffer.append(self.slope(player_var, data_buffer[-(self.numb_of_players + 1)]))
 			
 
 	def give_stats_to_player(self,stat_type):
+		"""
+		Give desired stats to a player. The stat they want depends on which playstyle they have
+		TODO scrap this
+		:param stat_type:
+		:return:
+		"""
 
 		schema = {'Stat Type':{'allowed': self.__stat_type}}
 		v = Validator(schema)
@@ -74,42 +99,54 @@ class Stats:
 
 		return 0
 
-	def shape_for_all_graph(self):
-
-		self.b_slope = self.shape_graph(self.bust_slopes_buffer,self.b_slope)
-		# self.busts = self.shape_graph(self.bust_buffer,self.busts)
-		# self.wins = self.shape_graph(self.wins_buffer,self.wins)
-		self.money = self.shape_graph(self.money_buffer,self.money)
-		# self.bet_size = self.shape_graph(self.bet_size_buffer,self.bet_size)
-
-	def shape_graph(self,buff,array):
+	def shape_graph(self, buff, array):
+		"""
+		shape the data list to match the data structure for plotting
+		:param buff: Buffers of this class (e.i : bust_slopes_buffer)
+		:param array: Something that will be transformed in numpy arrays for matplotlib
+		:return:
+		"""
 
 		array = np.asarray(buff)
-		return np.reshape(array,(len(buff)/self.numb_of_players,self.numb_of_players))
+		return np.reshape(array, (len(buff)/self.numb_of_players, self.numb_of_players))
+
+	def shape_for_all_graph(self):
+		"""
+		Calls the shape graph for the graphs that will be displayed
+		:return: NA
+		"""
+
+		self.b_slope = self.shape_graph(self.bust_slopes_buffer, self.b_slope)
+		# self.busts = self.shape_graph(self.bust_buffer,self.busts)
+		# self.wins = self.shape_graph(self.wins_buffer,self.wins)
+		self.money = self.shape_graph(self.money_buffer, self.money)
+		# self.bet_size = self.shape_graph(self.bet_size_buffer,self.bet_size)
 
 	def show_all(self):
+		"""
+		Fonction to show all the graphs and all the stats
+		:return: NA
+		"""
 
 		self.shape_for_all_graph()
 		names = self.bjgame.get_players_names()
 
 		# self.show_data("wins",names,self.wins,self.wins_buffer)
 		# self.show_data("Busts",names,self.busts,self.bust_buffer)
-		self.show_data("Bust Slope",names,self.b_slope,self.bust_slopes_buffer)
-		self.show_data("Money",names,self.money,self.money_buffer)
+		self.show_data("Bust Slope",names, self.b_slope, self.bust_slopes_buffer)
+		self.show_data("Money", names, self.money, self.money_buffer)
 		# self.show_data("bet size",names,self.bet_size,self.bet_size_buffer)
 
-	def show_data(self,label,names,points,buff):
+	def show_data(self, label, names, points, buff):
 		self.shape_graph(buff,points)
-		# self.shape_for_bust_slope()
 		try :
 			graph = ui.Graphs(label,points,names)
 			graph.start()
 		except ValueError:
 			raise(ValueError)
 
-	def slope(self,y2,y1):
-		 m = (y2-y1)
-		 return m
+	def slope(self, y2, y1):
+		return (y2-y1)
     	
 
 if __name__ == '__main__':
